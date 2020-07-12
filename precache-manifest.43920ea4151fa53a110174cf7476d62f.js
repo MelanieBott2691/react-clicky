85052f44d69aa9375cbd75fcd7ff51d10a798379
Melanie Bott <melaniebott@Melanies-iMac.hsd1.ut.comcast.net>
2020-07-11 23:16:30 -0600

    Updates

---
+++ precache-manifest.43920ea4151fa53a110174cf7476d62f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc55899572d06d500b323e1dbaf1c2d9",
+    "url": "/react-clicky/index.html"
+  },
+  {
+    "revision": "0945fdf7a68621195bbf",
+    "url": "/react-clicky/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e9a597136450b1dd837",
+    "url": "/react-clicky/static/js/2.4cd598cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clicky/static/js/2.4cd598cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0945fdf7a68621195bbf",
+    "url": "/react-clicky/static/js/main.745c9e06.chunk.js"
+  },
+  {
+    "revision": "843e16e4d3f8d4b8ebf6",
+    "url": "/react-clicky/static/js/runtime-main.c792c004.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-clicky/static/media/logo.5d5d9eef.svg"
+  }
+]);
